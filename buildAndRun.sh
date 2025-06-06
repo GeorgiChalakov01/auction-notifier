b4a07e4d061602f8313b5c657a49bb920b84e701
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-06-06 15:38:27 +0300

    added a persistant volume to the container.

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker buildx build -t auction-notifier-backend .
-docker run -d --name auction-notifier -p 5000:5000 -e VIRTUAL_HOST=auction.gchalakov.com -e LETSENCRYPT_HOST=auction.gchalakov.com --network net auction-notifier-backend
+docker run -d --name auction-notifier -p 5000:5000 -e VIRTUAL_HOST=auction.gchalakov.com -e LETSENCRYPT_HOST=auction.gchalakov.com --network net -v /home/gchalakov/services/auction-notifier:/app auction-notifier-backend
