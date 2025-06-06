a7e45b31d37741a664093f42d84bd4e68a5b5345
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-06-06 15:40:19 +0300

    added recreating of the container

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 docker buildx build -t auction-notifier-backend .
+
+docker stop auction-notifier
+docker rm auction-notifier
+
 docker run -d --name auction-notifier -p 5000:5000 -e VIRTUAL_HOST=auction.gchalakov.com -e LETSENCRYPT_HOST=auction.gchalakov.com --network net -v /home/gchalakov/services/auction-notifier:/app auction-notifier-backend
